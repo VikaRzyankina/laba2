0746636ecc46e1a0b58352f1dd7c6a696256a768
VikaRzyankina <124519094+VikaRzyankina@users.noreply.github.com>
2023-02-20 21:41:52 +0400

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,26 @@
+def repl(n):
+    f = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь',
+         9: 'девять'}
+    return f.get(n)  # замена из словаря
+uwu=[]
+kl=False
+import re
+f=open('text.txt')
+while True:
+    buff=f.readline()
+    if not buff:
+        if not kl:
+            print('Файл пуст')
+        break
+    u=re.findall(r'\b(?!\d*0{5}\d*0{5}\d*0{5}\d*)\d+\b',buff)
+    kl=True
+    uwu+=u
+if len(uwu) == 0:
+    print('В файле нет подходящих под условие чисел')
+    quit()
+for j in uwu: #замена чисел прописью
+    exp = []
+    for i in j:
+        if repl(int(i)) not in exp:
+            exp.append(repl(int(i)))
+    print(j + ' - ', *exp)
